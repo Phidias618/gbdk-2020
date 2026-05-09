c68adc824b469327be9cb06c06ee7590ee613c78
Phidias618 <126189685+Phidias618@users.noreply.github.com>
2026-05-09 23:47:45 +0200

    Update get_xy_t.s

--- gbdk-lib/libc/targets/sm83/get_xy_t.s
+++ gbdk-lib/libc/targets/sm83/get_xy_t.s
@@ -16,71 +16,70 @@
         AND     #LCDCF_BG9C00
         JR      NZ,.is9c
 .is98:
-        LD      HL,#0x9800
+        LD      H,#0x98
         JR      .get_xy_tt
 .is9c:
-        LD      HL,#0x9C00
+        LD      H,#0x9C
         
-        ;; Store background tile table into (BC) at XY = DE, size WH on stack, from vram from address (HL)
+        ;; Store background tile table into (BC) at XY = DE, size WH on stack, from vram from address (H << 8)
 .get_xy_tt::
-        PUSH    BC              ; Store source
+        ld l, d
+        ld a, e
+        add a
+        add a
+        add a
+        add a
+        ld d, #0
+        rr d
+        ld e, a
+        add hl, de
+        add hl, de
+        ld d, h
+        ld e, l                 ; source de = (H << 8) + 0x20 * Y + X
 
-        SWAP    E
-        RLC     E
-        LD      A,E
-        AND     #0x03
-        ADD     H
-        LD      B,A
-        LD      A,#0xE0
-        AND     E
-        ADD     D
-        LD      C,A             ; dest BC = HL + 0x20 * Y + X
-
-        POP     HL              ; HL = source
-        POP     DE              ; DE = WH
-        PUSH    DE              ; store WH
-        PUSH    BC              ; store dest
-
-3$:                             ; Copy W tiles
-
+        ld h, b
+        ld l, c                 ; HL = destination
+        
+        pop bc                  ; BC = WH
+0$:
+        push bc                 ; store WH
+        push de                 ; store source
+        ; Copy W tiles
+1$:                             
         WAIT_STAT
-        LD      A, (BC)
-        LD      (HL+), A
+        ld a, (de)
+        ld (hl+), A
+
+        ; inc source and wrap around
+        inc e
+        ld a, e
+        and #0x1F
+        jr nz, 2$
+        ld a, e
+        sub #0x20
+        ld e, a
+2$:
+        dec b
+        jr nz, 1$
+
+        pop de
+        pop bc
+
+        dec c
+        ret z
+
+        ; next row and wrap around
+        ld a, e
+        add #32 
+        ld e, a
+        jr nc, 0$
+
+        ld a, d
+        rrca
+        rrca
+        add #64
+        rlca
+        rlca
+        ld d, a
         
-        LD      A, C            ; inc dest and wrap around
-        AND     #0xE0
-        LD      E, A
-        LD      A, C
-        INC     A
-        AND     #0x1F
-        OR      E
-        LD      C, A
-
-        DEC     D
-        JR      NZ, 3$
-
-        POP     BC
-        POP     DE
-
-        DEC     E
-        RET     Z
-
-        PUSH    DE
-
-        LD      A, B            ; next row and wrap around
-        AND     #0xFC
-        LD      E, A            ; save high bits
-
-        LD      A,#0x20
-
-        ADD     C
-        LD      C, A
-        ADC     B
-        SUB     C
-        AND     #0x03
-        OR      E               ; restore high bits
-        LD      B, A
-
-        PUSH    BC
-        
-        JR      3$
+        jr 0$
